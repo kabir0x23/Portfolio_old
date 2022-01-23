8eee8da1d3357941d5662c80d4d3171df461f0fe
kabir0x17 <kabirj2310@gmail.com>
2022-01-23 20:05:49 +0530

    Update TimeLine.js

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -12,7 +12,7 @@ const Timeline = () => {
     const carouselRef = useRef();
 
     const scroll = (node, left) => {
-      return node.scrollTo({ left, behavior: 'smooth' });
+        return node.scrollTo({left, behavior: 'smooth'});
     }
 
     const handleClick = (e, i) => {
@@ -44,6 +44,7 @@ const Timeline = () => {
     }, []);
 
     return (
+
         <Section id={"about"}>
             <SectionTitle>About Me</SectionTitle>
             <SectionText>
