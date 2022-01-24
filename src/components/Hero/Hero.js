ffa2daec0e5bd08017bf744d13336587777020a3
kabir0x17 <kabirj2310@gmail.com>
2022-01-24 19:56:54 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,22 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {Section, SectionText, SectionTitle} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {LeftSection} from './HeroStyles';
 
 const Hero = () => (
-<Section row nopadding>
-    <LeftSection>
-        <SectionTitle main center>
-            Welcome to <br />
-            My Personal Portfolio
-        </SectionTitle>
-        <SectionText>
-            I am Cyber Security Enthusiast, a Full Stack Web Developer and a Programmer
-        </SectionText>
-        <Button onClick={() => window.location = 'https://linktree.com/kabir0x17'}>Learn More</Button>
-
-
-    </LeftSection>
-</Section>
+    <Section row nopadding>
+        <LeftSection>
+            <SectionTitle main center>
+                Welcome to <br/>
+                My Personal Portfolio
+            </SectionTitle>
+            <SectionText>
+                I am Cyber Security Enthusiast, a Full Stack Web Developer and a Programmer
+            </SectionText>
+            <Button onClick={() => window.location = 'https://linktree.com/kabir0x17'}>Learn More</Button>
+        </LeftSection>
+    </Section>
 );
 
 export default Hero;
