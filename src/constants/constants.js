b0daee7693a1bde76407412e6edcc6672c1aafcf
kabir0x17 <kabirj2310@gmail.com>
2022-02-02 23:54:00 +0530

    update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -40,7 +40,7 @@ export const projects = [
 export const TimeLineData = [
     {year: 2017, text: 'Started Diploma in Computer Science',},
     {year: "2018-2019", text: 'Enjoyed College Life',},
-    {year: 2020, text: 'Started Learning about Cyber Security and Founded TheCyberWorld',},
+    {year: 2020, text: 'Started Learning about Cyber Security, Founded TheCyberWorld and Started B-TECH',},
     {year: 2021, text: 'Started posting content on platform',},
     {year: 2022, text: 'Started Youtube',},
 ];
