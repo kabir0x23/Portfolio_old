5324f7d34a2d2c58a248499eec4ce4c45f5a65b5
kabir0x17 <kabirj2310@gmail.com>
2022-04-08 13:05:26 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,8 +29,8 @@ const Projects = () => (
                         </TagList>
                     </div>
                     <UtilityList>
-                        <ExternalLinks href={visit}>Code</ExternalLinks>
-                        <ExternalLinks href={source}>Source</ExternalLinks>
+                        <ExternalLinks href={visit}>Live</ExternalLinks>
+                        <ExternalLinks href={source}>Source Code</ExternalLinks>
                     </UtilityList>
                 </BlogCard>
             ))}
