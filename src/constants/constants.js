19e8b3ec8f5b9c11c2c2aa6445b24749fe974cdb
kabir0x17 <kabirj2310@gmail.com>
2022-04-26 13:18:17 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         title: 'NewsFlash',
-        description: "NewsFlash is going to provide various news by using API.",
+        description: "NewsFlash is News Website, NewsFlash will going to provide News using api in future.",
         image: '/images/img.png',
         tags: ['Html', 'CSS', 'JavaScript'],
         source: 'https://github.com/kabir0x23/NewsFlash',
@@ -42,27 +42,27 @@ export const TimeLineData = [
     {
         year: 2020,
         text: <ul>
-            <li>1. Build a Blog Website with PHP</li>
-            <li>2. Started Learning about Cyber Security.</li>
-            <li>3. Founded TheCyberWorld.</li>
-            <li>4. Started B-TECH.</li>
+            <li>1. Started learning about Html, CSS, Javascript and PHP</li>
+            <li>2. Build a Blog Website with PHP</li>
+            <li>3. Started Learning about Cyber Security.</li>
+            <li>4. Founded TheCyberWorld.</li>
+            <li>5. Started B-TECH.</li>
         </ul>
     },
     {
         year: 2021,
         text: <ul>
-            <li>1. Started learning about Html, CSS, Javascript</li>
-            <li>2. Started learning about Python</li>
-            <li>4. Started building thecyberworld community</li>
-            <li>4. Guided 20,000+ people.</li>
+            <li>1. Started learning about Python</li>
+            <li>2. Started building thecyberworld community</li>
+            <li>3. Guided 20,000+ people.</li>
         </ul>
     },
     {
         year: 2022, text:
             <ul>
-                <li>1. Started learning about Mern Stack</li>
-                <li>2. Started learning about AI</li>
-                <li>3. Started Youtube</li>
+                <li>1. Started learning about MERN Stack, DevOps and AI</li>
+                <li>2. Build a NewsFlash.</li>
+
             </ul>
     },
 ];
