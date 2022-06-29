e7e339eb786afdeec760be3ca132d60a5d4b161e
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-29 19:06:47 +0530

    Update Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
+import Header from '../../../Portfolio/src/components/Header/Header'
 import { Container } from './LayoutStyles'
 
 export const Layout = ({children}) => {
